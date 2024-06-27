a8eb78f17248d7687eb61440a13fed679eece24c
Aditya Shankar <aditniru@gmail.com>
2024-06-27 12:23:35 +0000

    modified code to use svelte instead

---
+++ app/src/routes/+page.svelte
@@ -0,0 +1,16 @@
+<script>
+    import image from "$lib/images/meditating-enby.svg"
+</script>
+
+<a class="header-link" href="/">
+    <div class="header">
+        <img src="{image}" alt="a meditating non-binary person" height="80">
+        <h1>River's blog</h1>
+    </div>
+</a>
+
+<div class="content">
+    <div class="stuff book-review">
+        <a href="/book-reviews/book-1">Book take away #1 : The Happiness hypothesis by Jonathan Heidt</a>
+    </div>
+</div>
---
+++ docs/_app/immutable/entry/start.BQICelS6.js
@@ -0,0 +1,1 @@
+import{a as t}from"../chunks/entry.Dp0d4wMV.js";export{t as start};
