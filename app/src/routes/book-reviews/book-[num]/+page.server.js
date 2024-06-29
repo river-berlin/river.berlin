10e211d139b10802c5b085ef4bc799199b7f482a
Aditya Shankar <aditniru@gmail.com>
2024-06-29 23:00:36 +0000

    Removed unnecessary files and set code to use icon from markdown folder instead

--- app/src/routes/book-reviews/book-[num]/+page.server.js
+++ app/src/routes/book-reviews/book-[num]/+page.server.js
@@ -1,7 +1,6 @@
-import { error } from '@sveltejs/kit';
-
 // @ts-ignore
 export async function load({ params }) {
     const content = (await import(`$things/book-takeaways/book-${params.num}/content.md?raw`)).default;
-    return {content:content}
+    const icon = (await import(`$things/book-takeaways/book-${params.num}/icon.svg`)).default;
+    return {content:content, icon:icon}
 }
