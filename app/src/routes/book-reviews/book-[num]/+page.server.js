9b6c6b3779eb3f1e0e9be73636cf6a3c212a41e0
Aditya Shankar <aditniru@gmail.com>
2024-06-29 23:07:29 +0000

    built website, added showdown as a dependency

--- app/src/routes/book-reviews/book-[num]/+page.server.js
+++ app/src/routes/book-reviews/book-[num]/+page.server.js
@@ -1,6 +1,14 @@
+import showdown from "showdown"
+
 // @ts-ignore
 export async function load({ params }) {
-    const content = (await import(`$things/book-takeaways/book-${params.num}/content.md?raw`)).default;
+    const markdown = (await import(`$things/book-takeaways/book-${params.num}/content.md?raw`)).default;
     const icon = (await import(`$things/book-takeaways/book-${params.num}/icon.svg`)).default;
-    return {content:content, icon:icon}
+
+    // convert markdown
+    const converter = new showdown.Converter({metadata : true})
+    const markdownHTML = converter.makeHtml(markdown)
+    const metadata = converter.getMetadata()
+
+    return {markdown, icon, markdownHTML, metadata}
 }
--- docs/_app/immutable/entry/start.CQt2ThZw.js
+++
@@ -1,1 +0,0 @@
-import{a as t}from"../chunks/entry.B-6j5mcO.js";export{t as start};
