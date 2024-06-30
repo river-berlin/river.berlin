2d6773ce57334c32a37468c86ad1c766c569efc0
Aditya Shankar <aditniru@gmail.com>
2024-06-30 21:15:06 +0000

    modified book take away colors a lil bit

--- app/src/routes/+page.svelte
+++ app/src/routes/+page.svelte
@@ -9,7 +9,10 @@
 <div class="content">
     <div class="stuff book-review">
         {#each data.blogs as blog }
-            <a href="/book-reviews/book-{blog.num}"><span class="take-away">Book take away #{blog.num}</span> : {blog.metadata.title}</a>
+            <a href="/book-reviews/book-{blog.num}">
+                <img src="/scroll.svg" alt="" width="15">
+                <span class="take-away">Book take away #{blog.num}</span> : {blog.metadata.title}
+            </a>
         {/each}
     </div>
 </div>
