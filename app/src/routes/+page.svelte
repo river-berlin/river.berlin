e8001a422eb697fc6dd99afba0b0c2b3bf4ce392
Aditya Shankar <aditniru@gmail.com>
2024-06-30 16:03:44 +0000

    styled the 'book take away' part

--- app/src/routes/+page.svelte
+++ app/src/routes/+page.svelte
@@ -9,7 +9,7 @@
 <div class="content">
     <div class="stuff book-review">
         {#each data.blogs as blog }
-            <a href="/book-reviews/book-{blog.num}">Book take away #{blog.num} : {blog.metadata.title}</a>
+            <a href="/book-reviews/book-{blog.num}"><span class="take-away">Book take away #{blog.num}</span> : {blog.metadata.title}</a>
         {/each}
     </div>
 </div>
